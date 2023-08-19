64f419084b4c052ace3ed3240118a0dc988077ef
Tawanda Chandiwana <astleytac@gmail.com>
2023-08-19 12:51:22 +0200

    added Floating Action Button and ShareSheet

--- Eyedentify/View/ViewController.swift
+++ Eyedentify/View/ViewController.swift
@@ -18,13 +18,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     var classificationResults : [String] = []
     let imagePicker = UIImagePickerController()
     private var animationView: LottieAnimationView!
+    var symbolButton: SFSymbolButton!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        imagePicker.delegate = self
+        createFloatingActionButton()
+        setupAnimation()
         imageView.isHidden = true
         titleLabel.text = "SelectSource".localized
-        imagePicker.delegate = self
-        setupAnimation()
+        symbolButton = SFSymbolButton(symbolName: "square.and.arrow.up")
+        symbolButton.tintColor = .label
+        symbolButton.delegate = self
+        symbolButton.translatesAutoresizingMaskIntoConstraints = false
+        symbolButton.isHidden = true
+        view.addSubview(symbolButton)
+        symbolButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        symbolButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
         
     }
     
@@ -40,6 +51,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         hideAnimation()
+        symbolButton.isHidden = false
         if let pickedImage = info[.originalImage] as? UIImage {
             guard let image = CIImage(image: pickedImage) else {
                 return
@@ -57,141 +69,163 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
-extension ViewController {
+extension ViewController: SFSymbolButtonDelegate {
     
-    //MARK: Private methods
-    
-    private func detectImage(image: CIImage){
-        guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: .init()).model) else {
-            showAlertAndExit()
+    func FABTapped() {
+        symbolButton.isHidden = true
+        guard let screenshot = takeScreenshot() else {
+            symbolButton.isHidden = true
             return
         }
-        let request = VNCoreMLRequest(model: model) { [self] request, error in
-            guard let results = request.results as? [VNClassificationObservation] else {
-                fatalError()
-            }
-            if let firstResult = results.first {
-                let confidence = firstResult.confidence.rounded() * 100
+        symbolButton.isHidden = true
+        // Share the screenshot
+        let activityViewController = UIActivityViewController(activityItems: [screenshot], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    
+}
+
+func takeScreenshot() -> UIImage? {
+    let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+    let screenshot = renderer.image { _ in
+        view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        symbolButton.isHidden = true
+    }
+    return screenshot
+}
+
+@objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+    if let error = error {
+        Alert.showAlert(title: "Error".localized, message: "Error saving image: \(error.localizedDescription)")
+    } else {
+        Alert.showAlert(title: "Success", message: "Image saved successfully.")
+    }
+}
+//MARK: Private methods
+
+private func detectImage(image: CIImage){
+    guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: .init()).model) else {
+        showAlertAndExit()
+        return
+    }
+    let request = VNCoreMLRequest(model: model) { [self] request, error in
+        guard let results = request.results as? [VNClassificationObservation] else {
+            fatalError()
+        }
+        if let firstResult = results.first {
+            let confidence = firstResult.confidence.rounded() * 100
+            
+            if confidence  < 50 {
+                self.titleLabel.text = "\(firstResult.identifier.lowercased()) ??? \nConfidence: \(confidence)% 🫣"
+                self.titleLabel.font = .italicSystemFont(ofSize: 20)
+                self.titleLabel.backgroundColor = UIColor(white: 0.5, alpha: 0.8)
                 
-                if confidence  < 50 {
-                    self.titleLabel.text = "\(firstResult.identifier.lowercased()) ??? \nConfidence: \(confidence)% 🫣"
-                    self.titleLabel.font = .italicSystemFont(ofSize: 20)
-                    self.titleLabel.backgroundColor = UIColor(white: 0.5, alpha: 0.8)
-                    
-                } else {
-                    self.titleLabel.text = "\(firstResult.identifier.uppercased()) \nConfidence: \(confidence)% 😎"
-                    self.titleLabel.font = .boldSystemFont(ofSize: 20)
-                    self.titleLabel.textColor = .lightText
-                    self.titleLabel.backgroundColor = UIColor(white: 0.2, alpha: 0.5)
-                    self.titleLabel.layer.cornerRadius = 5
-                }
-                showAlert(title: "Error".localized, message: error?.localizedDescription)
+            } else {
+                self.titleLabel.text = "\(firstResult.identifier.uppercased()) \nConfidence: \(confidence)% 😎"
+                self.titleLabel.font = .boldSystemFont(ofSize: 20)
+                self.titleLabel.textColor = .lightText
+                self.titleLabel.backgroundColor = UIColor(white: 0.2, alpha: 0.5)
+                self.titleLabel.layer.cornerRadius = 5
             }
-        }
-        
-        let handler = VNImageRequestHandler(ciImage: image)
-        
-        do {
-            try handler.perform([request])
-        }
-        catch {
-            showAlert(title: "Error".localized, message: error.localizedDescription)
-        }
-        
-    }
-    
-    func showAlert(title: String?, message: String?, completion: (() -> Void)? = nil) {
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let rootViewController = windowScene.windows.first?.rootViewController else {
-            return
-        }
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK".localized, style: .default) { _ in
-            completion?()
-        }
-        alertController.addAction(action)
-        rootViewController.present(alertController, animated: true, completion: nil)
-    }
-    
-    private func openCamera() {
-        
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-            imagePicker.sourceType = .camera
-            imagePicker.cameraDevice = .rear
-            imagePicker.cameraFlashMode = .auto
-            imagePicker.mediaTypes = [UTType.image.identifier]
-            imagePicker.delegate = self // Don't forget this line
-            present(imagePicker, animated: true)
-        }
-        else {
-            showAlert(title: "Error".localized, message: "CameraError".localized)
+            Alert.showAlert(title: "Error".localized, message: error?.localizedDescription)
         }
     }
     
-    private func openGallery() {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
-            imagePicker.allowsEditing = true
-           // imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-            imagePicker.mediaTypes = [UTType.image.identifier as String]
-            //imagePicker.dismiss(animated: true)
-            self.present(imagePicker, animated: true)
-        }
-        else {
-            showAlert(title: "Warning".localized, message: "GalleryError".localized)
-        }
+    let handler = VNImageRequestHandler(ciImage: image)
+    
+    do {
+        try handler.perform([request])
+    }
+    catch {
+        Alert.showAlert(title: "Error".localized, message: error.localizedDescription)
     }
     
-    private func showAlertAndExit() {
-        let alertController = UIAlertController(
-            title: "Error",
-            message: "An error occurred. The app will now exit.",
-            preferredStyle: .alert
-        )
-        
-        let exitAction = UIAlertAction(title: "Exit", style: .destructive) { _ in
-            fatalError("Forced app termination")
-        }
-        alertController.addAction(exitAction)
-        present(alertController, animated: true, completion: nil)
-    }
+}
+
+
+private func openCamera() {
     
-    private func setupAnimation() {
-        animationView = .init(name: "scanAnimation")
-        animationView.frame = imageView.frame
-        animationView.contentMode = .scaleAspectFit
-        animationView.loopMode = .loop
-        animationView.animationSpeed = 1.5
-        view.addSubview(animationView)
-        animationView.play()
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+        imagePicker.sourceType = .camera
+        imagePicker.cameraDevice = .rear
+        imagePicker.cameraFlashMode = .auto
+        imagePicker.mediaTypes = [UTType.image.identifier]
+        imagePicker.delegate = self // Don't forget this line
+        present(imagePicker, animated: true)
     }
+    else {
+        Alert.showAlert(title: "Error".localized, message: "CameraError".localized)
+    }
+}
+
+private func openGallery() {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+        imagePicker.allowsEditing = true
+        // imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.mediaTypes = [UTType.image.identifier as String]
+        //imagePicker.dismiss(animated: true)
+        self.present(imagePicker, animated: true)
+    }
+    else {
+        Alert.showAlert(title: "Warning".localized, message: "GalleryError".localized)
+    }
+}
+
+private func showAlertAndExit() {
+    let alertController = UIAlertController(
+        title: "Error",
+        message: "An error occurred. The app will now exit.",
+        preferredStyle: .alert
+    )
     
-    private func hideAnimation() {
-        animationView.stop()
-        animationView.isHidden = true
-        imageView.isHidden = false
+    let exitAction = UIAlertAction(title: "Exit", style: .destructive) { _ in
+        fatalError("Forced app termination")
     }
+    alertController.addAction(exitAction)
+    present(alertController, animated: true, completion: nil)
+}
+
+private func setupAnimation() {
+    animationView = .init(name: "scanAnimation")
+    animationView.frame = imageView.frame
+    animationView.contentMode = .scaleAspectFit
+    animationView.loopMode = .loop
+    animationView.animationSpeed = 1.5
+    view.addSubview(animationView)
+    animationView.play()
+}
+
+private func hideAnimation() {
+    animationView.stop()
+    animationView.isHidden = true
+    imageView.isHidden = false
+}
+
+private func showAnimation() {
     
-    private func showAnimation() {
-        
-        titleLabel.text = "SelectSource".localized
-        animationView.play()
-        animationView.isHidden = false
-        imageView.isHidden = true
-    }
+    titleLabel.text = "SelectSource".localized
+    animationView.play()
+    animationView.isHidden = false
+    imageView.isHidden = true
+}
+
+private func addSwipeGestures() {
     
-    private func addSwipeGestures() {
-        
-        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(performGesture(_:)))
-        imageView.addGestureRecognizer(gesture)
-        imageView.isUserInteractionEnabled = true
-        
-    }
+    let gesture = UISwipeGestureRecognizer(target: self, action: #selector(performGesture(_:)))
+    imageView.addGestureRecognizer(gesture)
+    imageView.isUserInteractionEnabled = true
     
-    @objc private func performGesture(_ gesture: UISwipeGestureRecognizer) {
-        if gesture.direction == .left {
-            showAnimation()
-        } else if gesture.direction == .right{
-            showAnimation()
-        }
+}
+
+private func createFloatingActionButton() {
+    
+}
+
+@objc private func performGesture(_ gesture: UISwipeGestureRecognizer) {
+    if gesture.direction == .left {
+        showAnimation()
+    } else if gesture.direction == .up{
+        FABTapped()
     }
+}
 }
